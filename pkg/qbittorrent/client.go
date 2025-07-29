0d265f7cf5568b4bb698d2c02c61e27bef81dc17
Suyunmeng <69945917+Suyunmeng@users.noreply.github.com>
2025-07-29 21:08:47 +0800

    fix(qb):Configure HTTP client with connection pooling and fix resource leaks in qBittorrent client

--- pkg/qbittorrent/client.go
+++ pkg/qbittorrent/client.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"time"
 
 	"github.com/OpenListTeam/OpenList/v4/pkg/utils"
 )
@@ -35,9 +36,22 @@ func New(webuiUrl string) (Client, error) {
 	if err != nil {
 		return nil, err
 	}
+	
+	// 配置HTTP客户端以防止连接泄漏
+	transport := &http.Transport{
+		MaxIdleConns:        10,
+		MaxIdleConnsPerHost: 2,
+		IdleConnTimeout:     30 * time.Second,
+		DisableKeepAlives:   false, // 启用连接复用
+	}
+	
 	var c = &client{
-		url:    u,
-		client: http.Client{Jar: jar},
+		url: u,
+		client: http.Client{
+			Jar:       jar,
+			Transport: transport,
+			Timeout:   30 * time.Second, // 设置总体超时
+		},
 	}
 
 	err = c.checkAuthorization()
@@ -69,6 +83,7 @@ func (c *client) authorized() bool {
 	if err != nil {
 		return false
 	}
+	defer resp.Body.Close()
 	return resp.StatusCode == 200 // the status code will be 403 if not authorized
 }
 
@@ -82,6 +97,7 @@ func (c *client) login() error {
 	if err != nil {
 		return err
 	}
+	defer resp.Body.Close()
 
 	// check result
 	body := make([]byte, 2)
@@ -157,6 +173,7 @@ func (c *client) AddFromLink(link string, savePath string, id string) error {
 	if err != nil {
 		return err
 	}
+	defer resp.Body.Close()
 
 	// check result
 	body := make([]byte, 2)
@@ -271,6 +288,7 @@ func (c *client) GetInfo(id string) (TorrentInfo, error) {
 	if err != nil {
 		return TorrentInfo{}, err
 	}
+	defer response.Body.Close()
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
@@ -316,6 +334,7 @@ func (c *client) GetFiles(id string) ([]FileInfo, error) {
 	if err != nil {
 		return []FileInfo{}, err
 	}
+	defer response.Body.Close()
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
@@ -349,6 +368,7 @@ func (c *client) Delete(id string, deleteFiles bool) error {
 	if err != nil {
 		return err
 	}
+	defer response.Body.Close()
 	if response.StatusCode != 200 {
 		return errors.New("failed to delete qbittorrent task")
 	}
@@ -359,6 +379,7 @@ func (c *client) Delete(id string, deleteFiles bool) error {
 	if err != nil {
 		return err
 	}
+	defer response.Body.Close()
 	if response.StatusCode != 200 {
 		return errors.New("failed to delete qbittorrent tag")
 	}
